758c2477ca4ec2d15883ba31fea06ef544fa489f
Ivan Kozak <freeivanzw@gmail.com>
2022-12-31 00:26:29 +0200

    updade adaptive component style

--- gulpfile.js
+++ gulpfile.js
@@ -39,15 +39,15 @@ gulp.task('clean', async function() {
 });
 
 gulp.task('webp', async function() {
-	gulp.src(['src/img/*.jpg', 'src/img/*.png',])
+	gulp.src(['src/images/*.jpg', 'src/images/*.png',])
 	.pipe(webp())
-	.pipe(gulp.dest('src/img'));
+	.pipe(gulp.dest('src/images'));
 
 })
  
-gulp.task('img', function() {
-	return gulp.src('src/img/**/*.*')
-		.pipe(gulp.dest('dist/img'));
+gulp.task('images', function() {
+	return gulp.src('src/images/**/*.*')
+		.pipe(gulp.dest('dist/images'));
 });
  
 gulp.task('prebuild', async function() {
@@ -76,5 +76,5 @@ gulp.task('serve', function () {
 	gulp.watch('src/js/**/*.js').on("change", reload);
 });
 
-gulp.task('build', gulp.series('prebuild', 'clean',  'sass', 'scripts', 'plugins', 'img'));
+gulp.task('build', gulp.series('prebuild', 'clean',  'sass', 'scripts', 'plugins', 'images'));
 
